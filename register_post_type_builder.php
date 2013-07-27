e5c6a2f6d160591e54da74e37f293e5a216ae209
Brian S. Reed <gits@scottreeddesign.com>
2013-07-27 00:08:52 -0400

    added custom icon support and featured image bug

--- register_post_type_builder.php
+++ register_post_type_builder.php
@@ -1,8 +1,24 @@
 <?php
 
 function register_post_type_builder( $singular_title, $plural_title, $args_overrides = array(), $prefix = "" ) {
+	
+	$post_type = $prefix . str_replace( '-', '_', sanitize_key( $singular_title ));
+	
+	if(array_key_exists('icon', $args_overrides)){
+		
+		add_action( 'admin_head', function() use( &$post_type, &$args_overrides ) { ?><style type="text/css">
+			#menu-posts-<?php echo $post_type; ?> .wp-menu-image {
+				background: url(<?php echo $args_overrides['icon']; ?>) no-repeat 6px -17px !important;
+			}
+			#menu-posts-<?php echo $post_type; ?>:hover .wp-menu-image,
+			#menu-posts-<?php echo $post_type; ?>.wp-has-current-submenu .wp-menu-image {
+				background-position: 6px 7px !important;
+			}
+		</style><?php });
+	}
+	
 	add_action( 'init', function()  use( &$singular_title, &$plural_title, &$args_overrides, &$prefix) {
-		echo "<!-- prefix: " . $prefix . " -->";
+		
 		register_post_type( $prefix . str_replace( '-', '_', sanitize_key( $singular_title )), array_merge( array(
 		'labels' => array(
 			'name' => $plural_title,
